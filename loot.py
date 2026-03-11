3ae14d9ce4a6a9601bad3fcab2bf5569f7bfafbd
n0whereno1 <sean.m.holmes@outlook.com>
2026-03-11 10:28:19 -0700

    Forgot what i changed lol

--- loot.py
+++ loot.py
@@ -13,15 +13,6 @@ weapons = [
     {"name": "Apprentice Sword", "ar":12},
 ]
 
-#armors = [
- #   {"name": "Old Shirt", "ar":2},
-  #  {"name": "Leather Armor", "ar":5},
-   # {"name": "Iron Plate", "ar":10},
- #   {"name": "Steel Mail", "ar":15},
- #   {"name": "Wizard Robe", "ar":3},
- #   {"name": "Debug Hat", "ar":500},
-#]
-
 
 def random_loot():
     lootselect = random.randomint(0, 100)
